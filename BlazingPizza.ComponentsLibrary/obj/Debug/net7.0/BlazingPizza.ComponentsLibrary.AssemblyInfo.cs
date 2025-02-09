055088cb133146b667361567f948d282b41e17ae
PC <davidlejo2005.2@gmail.com>
2025-02-09 19:04:08 +0100

    undecimo commit
    ligeros cambios

--- BlazingPizza.ComponentsLibrary/obj/Debug/net7.0/BlazingPizza.ComponentsLibrary.AssemblyInfo.cs
+++ BlazingPizza.ComponentsLibrary/obj/Debug/net7.0/BlazingPizza.ComponentsLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza.ComponentsLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204db05cddae22775f045880b7a5a585a4b99042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2010487de0718d54dc3ac667ef21dabed9e02481")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza.ComponentsLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza.ComponentsLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlazingPizza.Server/obj/Debug/net7.0/BlazingPizza.Server.AssemblyInfo.cs
+++ BlazingPizza.Server/obj/Debug/net7.0/BlazingPizza.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204db05cddae22775f045880b7a5a585a4b99042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2010487de0718d54dc3ac667ef21dabed9e02481")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
